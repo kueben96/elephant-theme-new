12f4b0ec12d07504fa945f39c640f46f2fd37b4e
Kübra <kueben96@gmail.com>
2020-08-27 13:26:09 +0200

    add mobile css

--- app_trash.js
+++ app_trash.js
@@ -7,8 +7,10 @@ const elephant = document.getElementById('#elephant');
 const hdr_green = document.querySelector('.hdr-green');
 const section_general = document.querySelector('.general');
 const p_general_info = document.querySelector('.general-info');
-var body = document.body;
 
+
+
+var body = document.body;
 const tl = gsap.timeline();
 
 
@@ -22,8 +24,10 @@ var colors = [
 
   gsap.registerPlugin(ScrollTrigger);
   gsap.to(section_general, {
-    scrollTrigger: p_general_info,
-    //x: 300
+    scrollTrigger: body,
+    start: "top top",
+    end : "+=500",
+    scrub: 1
      // start the animation when ".box" enters the viewport (once)
     //x: 300
   });
@@ -49,7 +53,16 @@ tl.fromTo(hero, 1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut })
 .fromTo(slider, 1.2, {x:"-100%"}, {x:"0%", ease: Power2.easeInOut}, "-=1.2")
 //logo keine wirkung gerade, da image file, daher hamburger svg nutzen
 .fromTo(hamburger, 0.5, {opacity: 0, x:30}, {opacity: 1, x: 0})
-
+.to(body, {
+    backgroundImage:
+        "-webkit-linear-gradient(-4deg, " +
+        colors[0].left +
+        " 0%, " +
+        colors[1].middle +
+        " 50%, " +
+        colors[2].right +
+        " 90%)"
+})
 
 
 //-=1.2 damit die Animation fruher beginnt
