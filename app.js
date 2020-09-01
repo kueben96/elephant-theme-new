ad74a535dc03accd7ee365e150a7b3777b5267b3
Kübra <kueben96@gmail.com>
2020-09-01 18:48:55 +0200

    change to min-width

--- app.js
+++ app.js
@@ -7,52 +7,50 @@ const elephant = document.getElementById('#elephant');
 const hdr_green = document.querySelector('.hdr-green');
 const section_general = document.querySelector('.general');
 const p_general_info = document.querySelector('.general-info');
+const section_pakete = document.querySelector('.pakete');
 var body = document.body;
-
-const tl = gsap.timeline();
-
-
+var main = document.getElementsByTagName;
 
 var colors = [
-    { left: "#1abfea", middle: "#c471ed", right: "#f4515e" },
-    { left: "#45a247", middle: "#4c879b", right: "#4c879b" },
-    { left: "white", middle: "white", right: "white" },
-    { left: "#37ccfd", middle: "#b32fff", right: "#b32fff" }
+    //blau-blass-orange
+    { left: "#516663", middle: "#c1c1b5", right: "#e79d78" },
+    { left: "#e79d78", middle: "#916a4a", right: "#e9d29a" }
   ];
 
   gsap.registerPlugin(ScrollTrigger);
-  gsap.to(section_general, {
-    scrollTrigger: p_general_info,
-    //x: 300
-     // start the animation when ".box" enters the viewport (once)
-    //x: 300
+
+  let tl = gsap.timeline;
+ 
+  gsap.to(body,{
+    background: "linear-gradient(to left,#223A44, #92A8AC)",
+    scrollTrigger: {
+      trigger: hero,
+      start: "top",
+      markers: true,
+      toggleActions: "restart complete restart reset"
+    }
   });
 
-  /* //for (let i = 0; i < colors.length; i++) {
-    tl.to(body, 1, {
-      backgroundImage:
-        "-webkit-linear-gradient(-4deg, " +
-        colors[i].left +
-        " 0%, " +
-        colors[i].middle +
-        " 50%, " +
-        colors[i].right +
-        " 90%)"
-    });
-  } */
+  gsap.to(body,{
+    background: "linear-gradient(to left, #516663, #e79d78)",
+    scrollTrigger: {
+      trigger: section_general,
+      start: "top",
+      markers: true,
+      toggleActions: "restart complete restart reset"
+    }
+  });
+
+  gsap.to(body, {
+    background: "linear-gradient(to left, #e79d78, #e9d29a)",
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: section_pakete,
+      start: "top",
+      markers: true,
+      toggleActions: "restart complete restart reset"
+    }
+  });
+
 
   
-//where to start und where to end
-// tl.fromTo(hero, 1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut })
-// .fromTo(hdr_green, 1, {opacity: 0, x:30}, {opacity: 1, x:30})
-// .fromTo(elephant, 1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut }, "=-1")
-// .fromTo(slider, 1.2, {x:"-100%"}, {x:"0%", ease: Power2.easeInOut}, "-=1.2")
-// //logo keine wirkung gerade, da image file, daher hamburger svg nutzen
-// .fromTo(hamburger, 0.5, {opacity: 0, x:30}, {opacity: 1, x: 0})
-
-
-
-//-=1.2 damit die Animation fruher beginnt
-//if you wanna use multiple animations:
-//tl.fromTo(hero, 1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut })
-//.fromTo()
