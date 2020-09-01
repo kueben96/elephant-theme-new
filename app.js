12219b39ee2513bba2e3da06b1de05866bcaa570
Kübra <kueben96@gmail.com>
2020-09-01 20:08:55 +0200

    add navbar unresponsive

--- app.js
+++ app.js
@@ -11,6 +11,32 @@ const section_pakete = document.querySelector('.pakete');
 var body = document.body;
 var main = document.getElementsByTagName;
 
+const navSlide = () => {
+  const burger = document.querySelector('.hamburger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+  // toggle nav class
+  burger.addEventListener('click', () =>{
+    nav.classList.toggle('nav-active');
+
+    // Links animieren
+    navLinks.forEach((link, index) => {
+      console.log(index);
+      if ( link.style.animation) {
+        link.style.animation = '';
+      }
+      else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + 0.2}s`
+      }
+      
+    });
+  });
+  // links animieren
+}
+
+navSlide();
+
 var colors = [
     //blau-blass-orange
     { left: "#516663", middle: "#c1c1b5", right: "#e79d78" },
@@ -21,36 +47,36 @@ var colors = [
 
   let tl = gsap.timeline;
  
-  gsap.to(body,{
-    background: "linear-gradient(to left,#223A44, #92A8AC)",
-    scrollTrigger: {
-      trigger: hero,
-      start: "top",
-      markers: true,
-      toggleActions: "restart complete restart reset"
-    }
-  });
+  // gsap.to(body,{
+  //   background: "linear-gradient(to left,#223A44, #92A8AC)",
+  //   scrollTrigger: {
+  //     trigger: hero,
+  //     start: "top",
+  //     markers: true,
+  //     toggleActions: "restart complete restart reset"
+  //   }
+  // });
 
-  gsap.to(body,{
-    background: "linear-gradient(to left, #516663, #e79d78)",
-    scrollTrigger: {
-      trigger: section_general,
-      start: "top",
-      markers: true,
-      toggleActions: "restart complete restart reset"
-    }
-  });
+  // gsap.to(body,{
+  //   background: "linear-gradient(to left, #516663, #e79d78)",
+  //   scrollTrigger: {
+  //     trigger: section_general,
+  //     start: "top",
+  //     markers: true,
+  //     toggleActions: "restart complete restart reset"
+  //   }
+  // });
 
-  gsap.to(body, {
-    background: "linear-gradient(to left, #e79d78, #e9d29a)",
-    ease: "power3.out",
-    scrollTrigger: {
-      trigger: section_pakete,
-      start: "top",
-      markers: true,
-      toggleActions: "restart complete restart reset"
-    }
-  });
+  // gsap.to(body, {
+  //   background: "linear-gradient(to left, #e79d78, #e9d29a)",
+  //   ease: "power3.out",
+  //   scrollTrigger: {
+  //     trigger: section_pakete,
+  //     start: "top",
+  //     markers: true,
+  //     toggleActions: "restart complete restart reset"
+  //   }
+  // });
 
 
   
